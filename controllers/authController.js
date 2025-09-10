eefe6829ffba31db3629bc86a44b6101b042454c
Eslam-Elwey <eslam.elwey@gmail.com>
2025-09-11 01:53:29 +0300

    add login routing handler and middleware for checheking authenticity of user and authorization restrication to specific routes

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,7 +1,14 @@
+const{promisify} = require('util') ;
 const catchAsync = require("../utils/catchAsync");
 const User = require('../models/userModel.js') ;
 const jwt = require('jsonwebtoken');
 const AppError = require("../utils/appError.js");
+
+const signToken = (id) =>{
+    return jwt.sign({id} ,process.env.JWT_SECRET,{
+    expiresIn : process.env.JWT_EXPIRES_IN
+})
+}
 
 
 exports.signup = catchAsync(async(req,res,next)=>{
@@ -15,14 +22,9 @@ exports.signup = catchAsync(async(req,res,next)=>{
 
 }) ;
 
-if(!user)
-{
-    return new AppError('this email is already in use ... please use another one' , '404');
-}
+const token = signToken(user._id) ;
 
-const token = jwt.sign({id : user._id} ,process.env.JWT_SECRET,{
-    expiresIn : process.env.JWT_EXPIRES_IN
-})
+user.password = undefined;
 
     res.status(201).json({
         status:'success' ,
@@ -36,4 +38,80 @@ const token = jwt.sign({id : user._id} ,process.env.JWT_SECRET,{
 
 exports.login = catchAsync(async(req,res,next)=>{
 
+    const {email,password} = req.body ;
+
+    //check if email and password exist on the request 
+    if(!email || !password)
+    {
+        return next(new AppError('please provide email and password!...' , 400)) 
+    }
+
+
+    //check if email and password exist are correct
+    const user = await User.findOne({email})?.select('+password');
+    //compare passwords
+    const correct = await user?.correctPassword(password,user.password) ;
+
+    if(!user || !correct)
+    {
+        return next(new AppError('Incorrect email or password!...',401));
+    }
+
+    //correct email and password
+
+    const token = signToken(user._id) ;
+
+     res.status(200).json({
+        status:'success' ,
+        token,
+        role : user.role
+    })
+
+
+});
+
+//#region middleware
+exports.protectAuthenticatedRoutes = catchAsync(async(req,res,next)=>{
+
+    let token ;
+
+    //extracting token
+    if(req.headers.authorization && req.headers.authorization.startsWith('Bearer'))
+    {
+        token = req.headers.authorization.split(' ')[1] ;
+    }
+
+    if(!token)
+    {
+        return next(new AppError('You are not logged in! please log in to get access...',401))
+    }
+
+    // verification of token 
+    const decoded = await promisify(jwt.verify)(token,process.env.JWT_SECRET)
+    console.log(decoded)
+    //check if user is existed 
+    const matchedUser = await User.findById(decoded.id);
+    if(!matchedUser)
+    {
+        return next(new AppError('the user belongs to this token is no longer existes',401));
+    }
+
+    req.user = matchedUser ;
+
+    next() ;
 })
+
+
+exports.restrictTo = (...roles)=>{
+
+    return (req, res, next) => {
+    if (!roles.includes(req.user.role)) {
+      return next(
+        new AppError('You do not have permission to perform this action', 403)
+      );
+    }
+
+    next();
+  };
+}
+//#endregion
