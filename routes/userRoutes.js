131c876e67755d45309ae241f3b7ad158860273a
Eslam-Elwey <eslam.elwey@gmail.com>
2025-09-11 03:23:34 +0300

    add image middlewares related to upload and resize image

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,11 +1,16 @@
 const express = require('express') ;
 const authController = require('../controllers/authController') ;
 const userController = require('../controllers/userController') ;
+const { generateId } = require('../controllers/productController');
 
 const router = express.Router() ;
 
 
-router.post('/signup',authController.signup);
+router.post('/signup',generateId,
+            userController.uploadUserImage,
+            userController.resizeUserImage,
+            authController.signup);
+
 router.post('/login',authController.login);
 
 router.route('/')
@@ -14,17 +19,16 @@ router.route('/')
         userController.getAllusers)
 
 .post(  authController.protectAuthenticatedRoutes,
-        // authController.restrictTo('admin'),
+        authController.restrictTo('admin'),
+        generateId,
+        userController.uploadUserImage,
+        userController.resizeUserImage,
         userController.createUser) ;
 
 router.route('/:id')
 .get(authController.protectAuthenticatedRoutes,
     authController.restrictTo('admin'),
     userController.getUser)
-
-.patch( authController.protectAuthenticatedRoutes,
-        authController.restrictTo('admin'),
-        userController.updateUser)
 
 .delete(authController.protectAuthenticatedRoutes,
         authController.restrictTo('admin'),
