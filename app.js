ba8fb4e137e0d3d4165b2fb5000a702714db4f6e
Eslam-Elwey <eslam.elwey@gmail.com>
2025-09-13 08:41:14 +0300

    add cors setting and frontend doamin and options for cors

--- app.js
+++ app.js
@@ -1,10 +1,25 @@
 
 const express = require ('express') ;
 const path = require ('path') ;
+const cors = require ('cors') ;
 const productRouter = require('./routes/productRoutes') ;
 const userRouter = require('./routes/userRoutes') ;
 const globalErrorHandler = require('./controllers/errorController') ;
 const AppError = require('./utils/appError');
+
+const corsOptions = {
+  origin: (origin, callback) => {
+    const allowedOrigins = [process.env.FRONTEND_DOMAIN];
+    if (allowedOrigins.includes(origin) || !origin) {
+      callback(null, true);  // Allow request
+    } else {
+      callback(new Error('CORS not allowed'), false);  // Reject request
+    }
+  },
+  methods: ['GET', 'POST', 'PATCH', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+};
+
 
 
 const app = express() ;
@@ -12,6 +27,7 @@ const app = express() ;
 
 app.use(express.json()) ;
 app.use(express.static(path.join(__dirname, "public")));
+app.use(cors(corsOptions));
 
 
 //routes
