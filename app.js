8ec0ced74bfa687475e469880a40109c22d46a6a
Eslam-Elwey <eslam.elwey@gmail.com>
2025-09-13 06:50:28 +0300

    add middleware to serve images stored on backend server

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 
 const express = require ('express') ;
+const path = require ('path') ;
 const productRouter = require('./routes/productRoutes') ;
 const userRouter = require('./routes/userRoutes') ;
 const globalErrorHandler = require('./controllers/errorController') ;
@@ -10,6 +11,7 @@ const app = express() ;
 
 
 app.use(express.json()) ;
+app.use(express.static(path.join(__dirname, "public")));
 
 
 //routes
